5c1abbbec099b26d12a33b567d67cf9d135803fe
Bhavesh Goyal <bhavesh.goyal@zaggle.in>
2018-07-05 11:39:24 +0530

    Added example files

---
+++ Example/App.js
@@ -0,0 +1,76 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MultiSelectView from './index.1.js';
+var LoremIpsum = [
+  'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+  'adipiscing', 'elit', 'curabitur', 'vel', 'hendrerit', 'libero',
+  'eleifend', 'blandit', 'nunc', 'ornare', 'odio', 'ut',
+  'orci', 'gravida', 'imperdiet', 'nullam', 'purus', 'lacinia',
+  'a', 'pretium', 'quis', 'congue', 'praesent', 'sagittis', 
+  'laoreet', 'auctor', 'mauris', 'non', 'velit', 'eros',
+  'dictum', 'proin', 'accumsan', 'sapien', 'nec', 'massa',
+  'volutpat', 'venenatis', 'sed', 'eu', 'molestie', 'lacus',
+  'quisque', 'porttitor', 'ligula', 'dui', 'mollis', 'tempus',
+  'at', 'magna', 'vestibulum', 'turpis', 'ac', 'diam',
+  'tincidunt', 'id', 'condimentum', 'enim', 'sodales', 'in',
+  'hac', 'habitasse', 'platea', 'dictumst', 'aenean', 'neque',
+  'fusce', 'augue', 'leo', 'eget', 'semper', 'mattis', 
+  'tortor', 'scelerisque', 'nulla', 'interdum', 'tellus', 'malesuada',
+  'rhoncus', 'porta', 'sem', 'aliquet', 'et', 'nam',
+  'suspendisse', 'potenti', 'vivamus', 'luctus', 'fringilla', 'erat',
+  'donec', 'justo', 'vehicula', 'ultricies', 'varius', 'ante',
+  'primis', 'faucibus', 'ultrices', 'posuere', 'cubilia', 'curae',
+  'etiam', 'cursus', 'aliquam', 'quam', 'dapibus', 'nisl',
+  'feugiat', 'egestas', 'class', 'aptent', 'taciti', 'sociosqu',
+  'ad', 'litora', 'torquent', 'per', 'conubia', 'nostra',
+  'inceptos', 'himenaeos', 'phasellus', 'nibh', 'pulvinar', 'vitae',
+  'urna', 'iaculis', 'lobortis', 'nisi', 'viverra', 'arcu',
+  'morbi', 'pellentesque', 'metus', 'commodo', 'ut', 'facilisis',
+  'felis', 'tristique', 'ullamcorper', 'placerat', 'aenean', 'convallis',
+  'sollicitudin', 'integer', 'rutrum', 'duis', 'est', 'etiam',
+  'bibendum', 'donec', 'pharetra', 'vulputate', 'maecenas', 'mi',
+  'fermentum', 'consequat', 'suscipit', 'aliquam', 'habitant', 'senectus',
+  'netus', 'fames', 'quisque', 'euismod', 'curabitur', 'lectus',
+  'elementum', 'tempor', 'risus', 'cras'
+];
+
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <MultiSelectView/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
