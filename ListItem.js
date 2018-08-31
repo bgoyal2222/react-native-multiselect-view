94f475bd761f6a8a76ecff1b1e4e7beb4a3c90ed
Bhavesh Goyal <bhaveshgoyal999@gmail.com>
2018-08-31 12:56:11 +0530

    inactiveContainerStyle will be inherited by active Container when you pass in props

--- ListItem.js
+++ ListItem.js
@@ -19,7 +19,7 @@ export default class ListItem extends PureComponent {
 		const { text, activeContainerStyle, activeTextStyle, inactiveContainerStyle, inactiveTextStyle, checked, activeIcon, inactiveIcon } = this.props;
 		return (
 			<TouchableOpacity onPress={this.onTouch}>
-				{checked ? <View style={[styles.container, styles.activeContainer, activeContainerStyle]}>
+				{checked ? <View style={[styles.container, styles.activeContainer, inactiveContainerStyle, activeContainerStyle]}>
 					<Text numberOfLines={1} style={[styles.text, styles.activeText, activeTextStyle]}>{text}</Text>
 					{activeIcon ? activeIcon : <Ionicons name='md-close' size={16} style={[styles.icon, styles.activeText, activeTextStyle]} />}
 				</View> :
@@ -75,7 +75,83 @@ const styles = StyleSheet.create(
 		icon: {
 			marginTop: 3,
 			width: 20,
-
 		}
-
-	});
+	});import React, { PureComponent } from 'react';
+	import { View, StyleSheet, TouchableOpacity, Text, ViewPropTypes } from 'react-native';
+	import Ionicons from 'react-native-vector-icons/Ionicons';
+	import PropTypes from 'prop-types';
+	
+	export default class ListItem extends PureComponent {
+	
+		constructor(props) {
+			super(props);
+			this.onTouch = this.onTouch.bind(this);
+		}
+	
+		onTouch() {
+			const { checked, index, onTouch } = this.props;
+			onTouch(!checked, index);
+		}
+	
+		render() {
+			const { text, activeContainerStyle, activeTextStyle, inactiveContainerStyle, inactiveTextStyle, checked, activeIcon, inactiveIcon } = this.props;
+			return (
+				<TouchableOpacity onPress={this.onTouch}>
+					{checked ? <View style={[styles.container, styles.activeContainer, inactiveContainerStyle, activeContainerStyle]}>
+						<Text numberOfLines={1} style={[styles.text, styles.activeText, activeTextStyle]}>{text}</Text>
+						{activeIcon ? activeIcon : <Ionicons name='md-close' size={16} style={[styles.icon, styles.activeText, activeTextStyle]} />}
+					</View> :
+						<View style={[styles.container, inactiveContainerStyle]}>
+							<Text numberOfLines={1} style={[styles.text, inactiveTextStyle]}>{text}</Text>
+							{inactiveIcon ? inactiveIcon : <Ionicons name='md-add' size={16} style={[styles.icon, styles.text, inactiveTextStyle]} />}
+						</View>}
+				</TouchableOpacity>
+			);
+		}
+	}
+	
+	ListItem.propTypes = {
+		...TouchableOpacity.propTypes,
+		text: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+		activeContainerStyle: ViewPropTypes.style,
+		inactiveContainerStyle: ViewPropTypes.style,
+		activeTextStyle: Text.propTypes.style,
+		inactiveTextStyle: Text.propTypes.style,
+		checked: PropTypes.bool.isRequired,
+		activeIcon: PropTypes.element,
+		inactiveIcon: PropTypes.element,
+		onTouch: PropTypes.func.isRequired
+	}
+	
+	
+	const styles = StyleSheet.create(
+		{
+			container: {
+				paddingVertical: 5,
+				marginTop: 10,
+				marginRight: 10,
+				paddingLeft: 10,
+				flexDirection: 'row',
+				justifyContent: 'space-between',
+				alignItems: 'center',
+				borderWidth: 1,
+				borderRadius: 2,
+				backgroundColor: '#f8f8f8'
+			},
+			activeContainer: {
+				backgroundColor: '#BDD358',
+				borderColor: 'transparent'
+			},
+			activeText: {
+				color: '#fff'
+			},
+			text: {
+				fontSize: 16,
+				color: 'black',
+				paddingRight: 10
+			},
+			icon: {
+				marginTop: 3,
+				width: 20,
+			}
+		});
